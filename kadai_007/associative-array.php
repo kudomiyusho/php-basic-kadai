60889cbb5e60bb8555867f5a878bb90a7800a946
kudomiyusho <148519509+kudomiyusho@users.noreply.github.com>
2023-12-08 16:30:04 +0900

    課題007をアップデート

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+     <meta charset="UTF-8">
+     <title>PHP課題007</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // 連想配列
+        $products = [ 'name' => 'onion', 'price' => '200', 'weigh' => '160'];
+
+        // 連想配列の値を出力する
+        print_r($products);
+    
+        
+        
+        ?>
+    
+
+    </p>
+
+
+</body>
+
+
+
+
+</html>
