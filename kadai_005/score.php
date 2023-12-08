15c9460b8c496aaf530d13b2464e848791603c99
kudomiyusho <148519509+kudomiyusho@users.noreply.github.com>
2023-12-08 14:56:32 +0900

    課題005をアップロード

---
+++ kadai_005/score.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP課題005</title>
+ </head>
+ 
+ <body>
+    
+     <p>
+         <?php
+         $score = [80, 60, 55, 40, 100, 25, 80, 95, 30, 60];
+
+        
+         $average = array_sum($score) / count($score);
+
+
+
+        echo $average;
+         ?>
+     </p>
+ </body>
+ 
+ </html>
